6c212369890bccd102f4bf39503def3f9e701e8c
Thad Hutcheson <thadhutch@gmail.com>
2022-08-13 01:53:25 -0500

    Proxy Contract

---
+++ src/Proxy.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Proxy {
+
+    constructor(bytes memory constructData, address contractLogic) {
+
+        // Code in storage is keccak256("PROXIABLE")
+        assembly {
+            sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, contractLogic)
+        }
+
+        (bool success, ) = contractLogic.delegatecall(constructData);
+        require(success, "Build Failed");
+    }
+
+    fallback() external payable {
+        assembly {
+            let contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
+            calldatacopy(0x0, 0x0, calldatasize())
+            let success := delegatecall(sub(gas(), 10000), contractLogic, 0x0, calldatasize(), 0, 0)
+            let retSz := returndatasize()
+            returndatacopy(0, 0, retSz)
+            switch success
+            case 0 {
+                revert(0, retSz)
+            }
+            default {
+                return(0, retSz)
+            }
+        }
+    }
+}
